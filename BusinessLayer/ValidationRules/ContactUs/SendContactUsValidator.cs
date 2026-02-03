91785a6e93935078288a5365f0b38b00c996fdac
burakhaslak <burak_haslak_@hotmail.com>
2026-02-03 23:12:02 +0300

    postgresql

---
+++ BusinessLayer/ValidationRules/ContactUs/SendContactUsValidator.cs
@@ -0,0 +1,25 @@
+﻿using DTOLayer.DTOs.ContactDTOs;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules.ContactUs
+{
+    public class SendContactUsValidator : AbstractValidator<SendMessageDto>
+    {
+        public SendContactUsValidator()
+        {
+            RuleFor(x => x.Mail).NotEmpty().WithMessage("You can't leave this field empty.");
+            RuleFor(x => x.Subject).NotEmpty().WithMessage("You can't leave this field empty.");
+            RuleFor(x => x.Name).NotEmpty().WithMessage("You can't leave this field empty.");
+            RuleFor(x => x.MessageBody).NotEmpty().WithMessage("You can't leave this field empty.");
+            RuleFor(x => x.Subject).MinimumLength(5).WithMessage("You should write at least 5 characters.");
+            RuleFor(x => x.Subject).MaximumLength(100).WithMessage("You should write at most 100 characters.");
+            RuleFor(x => x.Mail).MinimumLength(5).WithMessage("You should write at least 5 characters.");
+            RuleFor(x => x.Mail).MaximumLength(100).WithMessage("You should write at most 100 characters.");
+        }
+    }
+}
--- TraversalCoreProje/Mapping/AutoMapperProfile/MapProfile.cs
+++ TraversalCoreProje/Mapping/AutoMapperProfile/MapProfile.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using DTOLayer.DTOs.AnnouncementDTOs;
 using DTOLayer.DTOs.AppUserDTOs;
+using DTOLayer.DTOs.ContactDTOs;
 using EntityLayer.Concrete;
 
 namespace TraversalCoreProje.Mapping.AutoMapperProfile
@@ -19,7 +20,7 @@ namespace TraversalCoreProje.Mapping.AutoMapperProfile
 
             CreateMap<AnnouncementUpdateDTO, Announcement>().ReverseMap();
 
-
+            CreateMap<SendMessageDto, ContactUs>().ReverseMap();
         }
     }
 }
